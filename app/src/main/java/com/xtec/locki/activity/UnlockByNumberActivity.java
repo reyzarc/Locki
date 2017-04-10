dc048661863d92225ac9f8b8fd8c29f53a95ad1d
hulong <hulong@yiqihao.com>
2017-04-10 17:30:00 +0800

    修复输入法导致的重复锁定bug

--- app/src/main/java/com/xtec/locki/activity/UnlockByNumberActivity.java
+++ app/src/main/java/com/xtec/locki/activity/UnlockByNumberActivity.java
@@ -1,9 +1,20 @@
 package com.xtec.locki.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xtec.locki.R;
+
 /**
  * Created by 武昌丶鱼 on 2017/3/24.
- * Description:
+ * Description:数字密码解锁
  */
 
-public class UnlockByNumberActivity {
+public class UnlockByNumberActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock_by_number_password);
+    }
 }
